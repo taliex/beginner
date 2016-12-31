8f4e4b1851666631edfb5991dc21ad6a352afbfd
sttai <taisiuting@gmail.com>
2016-12-31 22:41:54 +0800

    your message

---
+++ Assets/Scripts/GSM.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GSM : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject gsbtn;
+    public Text title;
+
+    private int state = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void buttonClicked() {
+
+        StartLevel();
+         
+
+    }
+
+    public void GameOver()
+    {
+        gsbtn.SetActive(true);
+        gsbtn.GetComponent<Text>().text = "Play Again";
+        title.text = "Game Over";
+    }
+    public void StartLevel()
+    {
+        Instantiate(player, Vector3.zero, Quaternion.identity);
+        title.text = "Level 1";
+        gsbtn.SetActive(false);
+      
+    }
+}
---
+++ Assets/Scripts/MoveByInput.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveByInput : MonoBehaviour {   
+
+    Rigidbody2D body;
+
+    public GameObject gsbtn;
+   
+
+    public float max_speed = 1f;
+    public float speed = 0.1f;
+    public float rpu = 1f;
+
+	// Use this for initialization
+	void Awake () {
+        body = GetComponent<Rigidbody2D>();
+
+            }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //stay in screen
+        if (transform.position.x > Camera.main.orthographicSize) {
+            transform.position = new Vector3(-Camera.main.orthographicSize, transform.position.y);
+        }
+        if (transform.position.x < -Camera.main.orthographicSize) {
+            transform.position = new Vector3(Camera.main.orthographicSize, transform.position.y);
+        }
+        if (transform.position.y > Camera.main.orthographicSize) {
+            transform.position = new Vector3(transform.position.x,-Camera.main.orthographicSize);
+        }
+        if (transform.position.y < -Camera.main.orthographicSize) {
+            transform.position = new Vector3(transform.position.x, Camera.main.orthographicSize);
+        }
+        
+        //input and move
+        if (Input.GetKey(KeyCode.W)) {
+
+            if (Vector3.Dot(body.velocity, transform.up) < max_speed)
+            {
+                body.AddForce(transform.up * speed);
+            }
+            else {
+                
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Vector3.Dot(body.velocity,transform.up) > 0)
+            {
+                body.AddForce(transform.up* speed * -1);
+            }
+            else {
+                body.velocity = Vector2.zero;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.Rotate(new Vector3(0,0,1),-rpu);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0,0,1), rpu);
+        }
+              
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider == true) {
+            
+        
+
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            gsbtn.GetComponent<GSM>().GameOver();
+        }
+    }
+    
+}
